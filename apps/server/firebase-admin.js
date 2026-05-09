201c516c11a593036c2088a9b76ee645da4c4876
unknown <saddiq.musa@miva.edu.ng>
2026-05-09 16:47:16 +0100

    fix: firebase-admin supports FIREBASE_SERVICE_ACCOUNT env, explicit dotenv path

--- apps/server/firebase-admin.js
+++ apps/server/firebase-admin.js
@@ -2,13 +2,17 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+  let cred;
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    cred = admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT));
+  } else if (process.env.FIREBASE_PROJECT_ID && process.env.FIREBASE_CLIENT_EMAIL && process.env.FIREBASE_PRIVATE_KEY) {
+    cred = admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-    })
-  });
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    });
+  }
+  if (cred) admin.initializeApp({ credential: cred });
 }
 
 module.exports = admin;
